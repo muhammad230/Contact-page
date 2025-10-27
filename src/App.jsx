93709c49ebd59384d2d88c9f65cecb668f306700
muhammad230 <ali.khansmr3@gmail.com>
2025-10-27 22:35:02 +0500

    add h1 and p

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Navigation from './components/Navigation'
+import contactHeader from './components/contactHeader'
 
 function App() {
 
   return (
   <div>
     <Navigation/>
+    <contactHeader/>
  </div>
  )
    
