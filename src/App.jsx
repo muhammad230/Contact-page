1a2e365b3c3df22e3d4863d6035fea4f75dc7fe2
muhammad230 <ali.khansmr3@gmail.com>
2025-10-29 22:27:41 +0500

    using css module in header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Navigation from './components/Navigation'
+import Navigation from './components/Navigation/Navigation'
 import contactHeader from './components/contactHeader'
 
 function App() {
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,6 +1,10 @@
+import styles from "./Navigation.module.css"
+
 const Navigation = () => {
+  console.log(styles);
+  
   return (
-   <nav className="container">
+   <nav className={`${styles.navigation} container`}>
     <div className="logo">
         <img src="/images/Frame 2 1.png" alt="logo" />
     </div>
