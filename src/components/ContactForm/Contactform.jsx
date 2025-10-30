8813698f858bd2f25af98793fb4a897f7ab245bb
muhammad230 <ali.khansmr3@gmail.com>
2025-10-30 20:15:49 +0500

    add csss module

---
+++ src/components/ContactForm/Contactform.jsx
@@ -0,0 +1,11 @@
+import styles from "./Contactform.module.css"
+
+const Contactform = () => {
+  return <section className={styles.container}>
+    <div className={styles.contact_form}></div>
+    <div className={styles.contact_image}></div>
+
+  </section>
+}
+
+export default Contactform
