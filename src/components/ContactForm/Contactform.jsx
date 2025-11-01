777fe1a1228fbaab1caf7522c678735fdcf44e06
muhammad230 <ali.khansmr3@gmail.com>
2025-11-01 19:25:57 +0500

    add image but not add css to it

--- src/components/ContactForm/Contactform.jsx
+++ src/components/ContactForm/Contactform.jsx
@@ -7,37 +7,45 @@ import { HiMail } from "react-icons/hi";
 const Contactform = () => {
   return (
     <section className={styles.container}>
-      <div className={styles.contact_form}></div>
-      <div className={styles.contact_image}></div>
-      <div className={styles.top_btn}>
-        <Button text="VIA SUPPORT CHAT" icon={<MdMessage fontSize="24px" />} />
-        <Button text="VIA CALL" icon={<MdLocalPhone fontSize="24px" />} />
-      </div>
-       <Button 
-       isnew ={true}
-       text="VIA EMAIL FORM" icon={<HiMail fontSize="24px" />} />
+      <div className={styles.contact_form}>
+        <div className={styles.top_btn}>
+          <Button
+            text="VIA SUPPORT CHAT"
+            icon={<MdMessage fontSize="24px" />}
+          />
+          <Button text="VIA CALL" icon={<MdLocalPhone fontSize="24px" />} />
+        </div>
+        <Button
+          isnew={true}
+          text="VIA EMAIL FORM"
+          icon={<HiMail fontSize="24px" />}
+        />
 
-       <form>
-        <div className={styles.form_control}>
+        <form>
+          <div className={styles.form_control}>
             <label htmlFor="name">Name</label>
             <input type="text" name="name" />
-        </div>
-        <div className={styles.form_control}>
+          </div>
+          <div className={styles.form_control}>
             <label htmlFor="email">Email</label>
             <input type="text" name="name" />
-        </div>
-        <div className={styles.form_control}>
+          </div>
+          <div className={styles.form_control}>
             <label htmlFor="name">Text</label>
-            <textarea name="text"/>
-        </div>
-        <div style={{
-          display:'flex',
-          paddingLeft:'230px',
-        }}>
-          <Button 
-       text="Submit" icon={<HiMail fontSize="24px" />} />
-        </div>
-       </form>
+            <textarea name="text" />
+          </div>
+          <div
+            style={{
+              display: "flex",
+              paddingLeft: "230px",
+            }}
+          > 
+            <Button text="Submit" icon={<HiMail fontSize="24px" />} />
+          </div>
+        </form>
+        <div className={styles.contact_image}>
+        <img src="/images/img.svg" alt="" /> </div>
+      </div>
     </section>
   );
 };
